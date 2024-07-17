e0adb61c86c99d395e13840e80c809bad63866cc
abhinav <abhinavsingh8477@gmail.com>
2024-07-17 11:03:14 +0000

    Initial commit: Added environments structure and cloudbuild.yaml

---
+++ environments/dev/provider.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project     = "sukrit-singh-426716"
+  region      = "us-central1"  
+}
---
+++ environments/prod/main.tf
@@ -0,0 +1,25 @@
+resource "google_compute_instance" "myvm" {
+  name         = "mynewtb2223333"
+  zone         = "us-east1-b"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "bucket67ut"
+    prefix = "terraform/state/prod"
+  }
+}
